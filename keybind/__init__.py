0bb9321fbdc250ba087a5261175a513f9bbb4e55
idle sign <idlesign@yandex.ru>
2018-11-15 20:38:07 +0700

    Release v0.1.0

--- keybind/__init__.py
+++ keybind/__init__.py
@@ -1,10 +1,9 @@
 from .binder import KeyBinder, configure_logging
 
 
-VERSION = (0, 0, 0)
+VERSION = (0, 1, 0)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
 """Application version number string."""
-
 
